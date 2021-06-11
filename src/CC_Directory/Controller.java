6ff4cd723b0f36c1cec2c0fd0c53bb3a21a24b85
Ethan <enephew2@mail.valenciacollege.edu>
2021-06-11 19:49:41 -0400

    Adding calculations with comboboxes.

--- src/CC_Directory/Controller.java
+++ src/CC_Directory/Controller.java
@@ -12,6 +12,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 /***
@@ -24,12 +25,20 @@ public class Controller implements Initializable, CONSTANTS {
     String EUR = WebReader.getPage(CONSTANTS.euro);
     String convertCOPtoUSD = String.format("%.5f", 1/Double.parseDouble(COP));
     String convertEURtoUSD = "";
+    HashMap<String, String> currencyHashMap = new HashMap<>();
 
     @FXML
     private Label USDtoCOP, COPtoUSD, USDtoEuro;
 
     @FXML
     private ComboBox comboBox1, comboBox2;
+
+
+    public void generateHashMap(){
+        currencyHashMap.put("USD", "1");
+        currencyHashMap.put("COP", WebReader.getPage(CONSTANTS.colombianPeso));
+        currencyHashMap.put("EUR", WebReader.getPage(CONSTANTS.euro));
+    }
 
     /***
      * A method that takes the string values of two currencies and returns the ratio.
@@ -42,12 +51,23 @@ public class Controller implements Initializable, CONSTANTS {
         return Z;
     }
 
+    /***
+     *
+     * @param comboBox1
+     * @param comboBox2
+     */
     public void getString(String comboBox1, String comboBox2){
-        
+        String output = "";
+        if(currencyHashMap.containsKey(comboBox1) && currencyHashMap.containsKey(comboBox2)){ // this should always be the case.
+            String comboVal1 = currencyHashMap.get(comboBox1);
+            String comboVal2 = currencyHashMap.get(comboBox2);
+            System.out.println(comboBox1 + " " + comboVal1 + " " + comboBox2 + " " + comboVal2);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        generateHashMap();
         USDtoCOP.setText("1 USD = " + COP + " COP");
         COPtoUSD.setText("1 COP = " + convertCOPtoUSD + " USD");
         USDtoEuro.setText("1 USD = " + EUR + " EUR");
@@ -55,9 +75,8 @@ public class Controller implements Initializable, CONSTANTS {
         comboBox2.getItems().addAll("COP", "EUR", "USD");
         comboBox1.getSelectionModel().selectFirst();
         comboBox2.getSelectionModel().selectFirst();
-
-        comboBox1.setOnAction(e -> System.out.println(comboBox1.getValue() + " " + comboBox2.getValue()));
-        comboBox2.setOnAction(e -> System.out.println(comboBox1.getValue() + " " + comboBox2.getValue()));
+        comboBox1.setOnAction(e -> getString(comboBox1.getValue().toString(), comboBox2.getValue().toString()));
+        comboBox2.setOnAction(e -> getString(comboBox1.getValue().toString(), comboBox2.getValue().toString()));
     }
 
 
