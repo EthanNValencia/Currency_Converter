742643fb32002f62e3422d661fe920a3c7005b79
Ethan <enephew2@mail.valenciacollege.edu>
2021-06-21 14:58:28 -0400

    Adding in some more project objectives.

--- src/CC_Directory/Main.java
+++ src/CC_Directory/Main.java
@@ -13,12 +13,12 @@ The server side will include a database
 The server side will store rates
 The server side will store rates with the time the rate was collected
 The client side will have the ability to display the historical rates
-in a useful way
+in a useful way, historical rates will be received from the server.
 
 What about adding in the unique currency symbols? $ (USD), £ (GBP), ₽ (RUB), etc
 
 In the inputText area, make it so that a user can input comas: 3,600 for example
-This will probably mean replacing commas with closed spaces. 
+This will probably mean replacing commas with closed spaces.
 */
 
 package CC_Directory;
